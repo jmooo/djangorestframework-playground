71d66526a8e0e1e9ae230403f9b60dcfaba961b5
Justin Montgomery <justin@justinmontgomery.com>
2018-02-12 10:21:32 -0800

    work through django-rest-framework tutorial

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # djangorestframework-playground
 New-Boot-Goofin' with the Django REST Framework
+
+
+## Setup
+* ``mkvirtualenv --python=`which python3` rest`` - Create new python3 venv, for when homebrew wrecks yours
+* `pip install -r requirements.txt`
+
+
+## Guides
+
+* http://www.django-rest-framework.org/tutorial/quickstart/
+* http://www.django-rest-framework.org/tutorial/1-serialization/
+* https://pymemcache.readthedocs.io/en/latest/getting_started.html#basic-usage
---
+++ snippets/urls.py
@@ -0,0 +1,44 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from snippets import views
+
+
+router = DefaultRouter()
+router.register('snippets', views.SnippetViewSet)
+router.register('users', views.UserViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
+
+
+
+# snippet_list = SnippetViewSet.as_view({
+#     'get': 'list',
+#     'post': 'create'
+# })
+# snippet_detail = SnippetViewSet.as_view({
+#     'get': 'retrieve',
+#     'put': 'update',
+#     'patch': 'partial_update',
+#     'delete': 'destroy'
+# })
+# snippet_highlight = SnippetViewSet.as_view({
+#     'get': 'highlight'
+# }, renderer_classes=[renderers.StaticHTMLRenderer])
+# user_list = UserViewSet.as_view({
+#     'get': 'list'
+# })
+# user_detail = UserViewSet.as_view({
+#     'get': 'retrieve'
+# })
+#
+# urlpatterns = format_suffix_patterns([
+#     url(r'^$', api_root),
+#     url(r'^snippets/$', snippet_list, name='snippet-list'),
+#     url(r'^snippets/(?P<pk>[0-9]+)/$', snippet_detail, name='snippet-detail'),
+#     url(r'^snippets/(?P<pk>[0-9]+)/highlight/$', snippet_highlight, name='snippet-highlight'),
+#     url(r'^users/$', user_list, name='user-list'),
+#     url(r'^users/(?P<pk>[0-9]+)/$', user_detail, name='user-detail')
+# ])
